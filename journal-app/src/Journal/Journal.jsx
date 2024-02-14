80302e503c9b438f6b28d485812e29eae71df6b4
marissarh <mbutto26@gmail.com>
2024-02-14 11:02:32 -0800

    update

--- journal-app/src/Journal/Journal.jsx
+++ journal-app/src/Journal/Journal.jsx
@@ -15,9 +15,6 @@ export default function Journal() {
                 setEntries(querySnapshot.docs)
             });
             setIsLoading(false)
-           
-            
-            
         }
         catch {
             setHasError(true)
