813dee07f69f3c3fa6cf1d64e5af3f2d55657f5b
marissarh <mbutto26@gmail.com>
2024-02-13 19:18:15 -0800

    update

--- journal-app/src/Journal/Journal.jsx
+++ journal-app/src/Journal/Journal.jsx
@@ -2,18 +2,24 @@ import React, {useEffect}from 'react';
 import db from '../db'
 
 export default function Journal() {
+    const [entries, setEntries] = useState([])
     useEffect(()=>{
         const getData =async()=>{
             const querySnapshot = await getDocs(collection(db, "journal-entries"));
-            querySnapshot.forEach((doc)=>{
-                console.log(doc.id, " => " , doc.data())
-            });
+            setEntries(querySnapshot.docs)
+            
         }
         getData();
     }, [])
     return (
         <div>
             <h1>Journal</h1>
+            {entries.map(entry =>{
+                return
+                <div key={entry.id}>
+                    {entry.data().entry}
+                </div>
+            })}
         </div>
     );
 }
