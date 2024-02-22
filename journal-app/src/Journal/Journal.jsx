aa7528129feef415fe658299a73cd71fbf631959
marissarh <mbutto26@gmail.com>
2024-02-21 16:33:37 -0800

    added firebase

--- journal-app/src/Journal/Journal.jsx
+++ journal-app/src/Journal/Journal.jsx
@@ -1,39 +1,13 @@
-import React, {useEffect, useState}from 'react';
-import db from '../db'
-import { onSnapshot } from 'firebase/firestore';
-import AddJournal from './AddJournal';
+import React from 'react';
+
 
 export default function Journal() {
-    const [entries, setEntries] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [hasError, setHasError] =useState(false);
-    useEffect(()=>{
-        const getData =async()=>{
-            try{
-            onSnapshot(collection(db, "journal-entries"), snapshot =>{
-                console.log("Current data: ", snapshot.docs);
-                setEntries(querySnapshot.docs)
-            });
-            setIsLoading(false)
-        }
-        catch {
-            setHasError(true)
-            setIsLoading(false)
-        }
-    }
+   
+    
         getData();
-    return () => onSnapshot;
-}
-    ,[])
-    return(
+    return (
         <div>
             <h1>Journal</h1>
-            {entries.map(entry =>{
-                return(
-                <div key={entry.id}>
-                    {entry.data().entry}
-                </div>)
-            })}
         </div>
     );
 }
