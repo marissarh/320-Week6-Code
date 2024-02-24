1d996b838455f8ae4557f5d2e55dac8bb95ce829
marissarh <mbutto26@gmail.com>
2024-02-23 18:14:11 -0800

    Update Journal.jsx

--- journal-app/src/Journal/Journal.jsx
+++ journal-app/src/Journal/Journal.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
 import db from '../db'
+import { collection, getDocs } from "firebase/firestore";
+
 
 export default function Journal() {
+    useEffect(()=>{
+
+const getData = async()=>{
+     const querySnapshot = await getDocs(collection(db, "journal-entries"));
+    querySnapshot.forEach((doc) => {
+   //doc.data() is never undefined for query doc snapshots
+  console.log(doc.id, " => ", doc.data());
+});
+
+}
+getData();
+
+    }, [])
+
+   
+
+
     return (
         <div>
             <h1>Journal</h1>
