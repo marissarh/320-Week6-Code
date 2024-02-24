0d9bd11eff02a0c78bf5f8150f96f331a98791a1
marissarh <mbutto26@gmail.com>
2024-02-24 15:40:30 -0800

    Update Journal.jsx

--- journal-app/src/Journal/Journal.jsx
+++ journal-app/src/Journal/Journal.jsx
@@ -1,14 +1,17 @@
-import React,{ useEffect } from 'react';
+import React,{ useEffect, useState } from 'react';
 import db from '../db'
 import { collection, getDocs } from "firebase/firestore";
 
 
 export default function Journal() {
+    const [entries, setEntries] = useState([])
     useEffect(()=>{
 
 const getData = async()=>{
      const querySnapshot = await getDocs(collection(db, "journal-entries"));
-    querySnapshot.forEach((doc) => {
+    console.log(querySnapshot.docs)
+     setEntries(querySnapshot.docs)
+     querySnapshot.forEach((doc) => {
    //doc.data() is never undefined for query doc snapshots
   console.log(doc.id, " => ", doc.data());
 });
