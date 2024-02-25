79561a72a6434e4c967e5f2e5d5a467db93be8c5
marissarh <mbutto26@gmail.com>
2024-02-24 17:35:47 -0800

    Add buttons
    Added delete and edit buttons

--- journal-app/src/Journal/Journal.jsx
+++ journal-app/src/Journal/Journal.jsx
@@ -1,32 +1,84 @@
 import React,{ useEffect, useState } from 'react';
 import db from '../db'
-import { collection, getDocs } from "firebase/firestore";
-
+import { collection, getDocs, doc, onSnapshot, deleteDoc, setDoc } from "firebase/firestore";
+import { Link } from 'react-router-dom';
+import AddJournal from './AddJournal';
+import { query, orderBy } from "firebase/firestore";  
+import JournalEntry from './JournalEntry';
 
 export default function Journal() {
-    const [entries, setEntries] = useState([])
+    const [entries, setEntries] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [hasError, setHasError] =useState(false);
+
+    const handleDelete = async(deleteEntry) =>{
+        console.log(deleteEntry)
+        await deleteDoc(doc(db, "journal-entries", deleteEntry)); 
+    }
+    const handleEdit = async (id) =>{
+        const newData = window.prompt('New Entry')
+        await setDoc(doc(db, 'journal-entries', id),{
+            entry: newData,
+            createdAt: new Date()
+        })
+    }
     useEffect(()=>{
 
 const getData = async()=>{
-     const querySnapshot = await getDocs(collection(db, "journal-entries"));
-    console.log(querySnapshot.docs)
-     setEntries(querySnapshot.docs)
-     querySnapshot.forEach((doc) => {
-   //doc.data() is never undefined for query doc snapshots
-  console.log(doc.id, " => ", doc.data());
-});
 
+    try{
+     //const querySnapshot = await getDocs(collection(db, "journal-entries"));
+    
+    //console.log(querySnapshot.docs) 
+   
+     //querySnapshot.forEach((doc) => {
+   //doc.data() is never undefined for query doc snapshots
+  //console.log(doc.id, " => ", doc.data());
+//});
+
+const journalQuery = query(collection(db, 'journal-entries'), orderBy("createdAt", 'desc'));
+
+onSnapshot(journalQuery, snapshot  => {
+    //console.log("Current data: ", snapshot.docs); 
+        setEntries(snapshot.docs)
+        
+    });
+    
+    setIsLoading(false)
+
+
+
+    } catch{
+        setHasError(true)
+        setIsLoading(false)
+    }
 }
 getData();
+return() => onSnapshot;
+
 
     }, [])
-
-   
-
+    if (isLoading){
+        return <p>Loading...</p>
+    }
+    if (hasError){
+        return <p>Error</p>
+    }
 
     return (
         <div>
             <h1>Journal</h1>
+            <AddJournal />
+            {entries.map(entry => {
+                return <div key={entry.id}>
+                    <p>{entry.data().entry}</p>
+                    <span>
+                    <Link to={`/journal/${entry.id}`}>View</Link>
+                    <button onClick={() =>{handleDelete(entry.id)}}>Delete</button>
+                    <button onClick={()=>{handleEdit(entry.id)}}>Edit</button>
+                    </span>
+                    </div>
+            })}
         </div>
     );
 }
